c31c5f6a76a15c4dc994576f82e8f7e61446dc55
Aditya Patnaik <adityapatnaik25x@gmail.com>
2020-08-02 19:47:34 +0530

    Added TabBar

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React from "react";
 import TopBar from './components/TopBar'
 import Main from './components/Main'
 import Footer from './components/Footer'
+import {makeStyles} from '@material-ui/styles'
 
+const useStyles = makeStyles({
+  root:{
+    background: '#1B2A40',
+  }
 
-export default function App() {
+})
+
+export default function App() {     
+
+  const classes = useStyles()
+
   return (
-     <>
-      <TopBar/>
+     <div className={classes.root}>
+      <TopBar title={"Aditya Patnaik"} sections={["Home","Blog","Projects","About"]}/>
       <Main/>
       <Footer/>
-     </>
+     </div>
    
   );
 }
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,27 +1,55 @@
 import React from "react";
-import { AppBar, Typography, Toolbar } from "@material-ui/core";
+import { AppBar, Typography, Toolbar, Grid,Tab,Tabs,Paper} from "@material-ui/core";
+import TabContext from '@material-ui/lab/TabContext';
+import TabList from '@material-ui/lab/TabList';
+import TabPanel from '@material-ui/lab/TabPanel';
 import { makeStyles } from "@material-ui/styles";
 
-const useStyles = makeStyles({
+
+const useStyles = makeStyles((theme)=>({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   appbar: {
-    alignItems: "center"
+    alignItems: "center",
+    background: '#1D2D40',
   }
-});
+}));
 
-const TopBar = () => {
+const TopBar = (props) => {
   const classes = useStyles();
+  const {title,sections} = props;
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
+
     <div className={classes.root}>
-      <AppBar className={classes.appbar} color="primary" position="sticky">
-        <Toolbar>
-          <Typography variant="h5">Aditya Patnaik</Typography>
-        </Toolbar>
+    <TabContext value={value}>
+      <AppBar position="static" className={classes.appbar}>
+        <TabList onChange={handleChange} aria-label="simple tabs example">
+          {
+            sections.map((section,key)=>{
+              return <Tab label={section} value={section} />
+            })
+          }
+        </TabList>
       </AppBar>
-    </div>
+      {
+          sections.map((section,key)=>{
+              return  <TabPanel value={section}>{key+1}</TabPanel>
+            })
+          }
+    </TabContext>
+  </div>
+
   );
 };
 
+
+
 export default TopBar;
+
